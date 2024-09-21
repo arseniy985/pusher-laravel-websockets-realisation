4468ff569b6b6019020bebfa540c974a1fb39cf5
Arseniy <arseniydruzhinin985@gmail.com>
2024-09-21 23:27:57 +0300

    added new message policies

--- app/Policies/MessagePolicy.php
+++ app/Policies/MessagePolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Message;
 use Illuminate\Support\Facades\Auth;
 
 class MessagePolicy
@@ -9,5 +10,15 @@ class MessagePolicy
     public function store(): bool
     {
         return Auth::check();
+    }
+
+    public function destroy(Message $message): bool
+    {
+        return Auth::check() && Auth::user()->id == $message->user_id;
+    }
+
+    public function edit(Message $message): bool
+    {
+        return Auth::check() && Auth::user()->id == $message->user_id;
     }
 }
